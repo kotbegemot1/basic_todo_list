b6b3724ddb5d4926a3096a441e3b32967ecc0891
kotbegemot1 <50358989+kotbegemot1@users.noreply.github.com>
2020-01-22 22:43:50 +0300

    new commit

---
+++ tests/test_pytest_1.py
@@ -0,0 +1,46 @@
+import pytest
+import sys
+
+def setup():
+    print ("Setup test")
+ 
+def teardown():
+    print ("Teardown test")
+
+def setup_module(module):
+    print ("Setup module")
+ 
+def teardown_module(module):
+    print ("Teardown module")
+ 
+def setup_function(function):
+    print ("Setup function")
+ 
+def teardown_function(function):
+    print ("Teardown function")
+ 
+def test_something_one():
+    print("Test 1")
+ 
+def test_something_two():
+    print("Test 2")
+
+@pytest.mark.xfail()
+def test_xfail():
+    assert False
+    
+@pytest.mark.xfail(
+    sys.version.startswith("3"),
+    reason="supported only in older Python versions")
+def test_xfail_condition():
+    assert False
+
+@pytest.mark.skipif(
+    sys.version.startswith("3"),
+    reason="supported only in older Python versions")
+def test_skipif():
+    assert False
+
+@pytest.mark.xfail()
+def test_xfail_fail():
+    assert True
